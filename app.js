d3261587a2222ee4cb200d9ea4b0339f93ec0a7d
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-12 12:33:46 -0700

    Answered problem number 2

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 // 2. Use a for loop that counts down from the max length of the word
 // 3. assign the count value to the indices value of the word
 
-function revWord(){
-    let word = prompt("Type in a word that you want reversed");
-    for(let i = 1; i < word.length+1; i++){
-        console.log(word[word.length-i]);
-    }
-}
+// function revWord(){
+//     let word = prompt("Type in a word that you want reversed");
+//     for(let i = 1; i < word.length+1; i++){
+//         console.log(word[word.length-i]);
+//     }
+// }
 
-asdf = revWord();
+// asdf = revWord();
 
+
+
+// 1. create a function that takes in a series of words as an input
+// 2. use a for loop that adds each letter into another word
+// 3. create logic in loop to capitalize a letter if the last letter was a " "
+// 4. return the new word
+
+// function capWords(){
+//     let oldWord = prompt("Please input a word");
+//     let newWord = "";
+//     for(let i = 0; i < oldWord.length; i++){
+//         let currentLetter = oldWord[i];
+//         if(oldWord[i-1] == " " || i == 0) newWord = newWord.concat(currentLetter.toUpperCase());
+//         else newWord = newWord.concat(currentLetter);
+//     }
+//     console.log(newWord);
+// }
+
+// fdsa = capWords()
